63f42a695cb4342dba2fc909203976af4a285ac1
crisostomon <72739646+crisostomon@users.noreply.github.com>
2021-05-24 16:27:49 +0100

    Removes old navigation code

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -21,38 +21,7 @@ fdt.onReady = function(ready) {
   });
 }
 
-// Find element that is currently pointing to the passed url by css class
-  fdt.findElementByClassUrl = function (cssclass, url) {
-  var elements = document.getElementsByClassName(cssclass);
-  var found = [];
-  for (var i = 0; i < elements.length; i++) {
-    // Check if element is the one that points to the passed url
-    if (elements[i].href === url && elements[i].classList.contains(cssclass)) {
-      found.push(elements[i]);
-    }
-  }
-
-  return found;
-};
-
-fdt.openMenus = function (menuElements) {
-  for (var i = 0; i < menuElements.length; i++) {
-    while (!menuElements[i].classList.contains("menu-root")) {
-      menuElements[i] = menuElements[i].parentNode;
-      // Check if the element has to be marked and mark it
-      if (menuElements[i].classList.contains("uk-parent")) {
-        menuElements[i].classList.add("uk-open");
-      }
-    }
-  }
-};
-
 function initNav() {
-  const menuElements = fdt.findElementByClassUrl("menu-link", window.location.href);
-
-  if (menuElements.length == 0) return;
-  fdt.openMenus(menuElements);
-
   {% if site.search_enabled != false and site.search.button %}
   const searchInput = document.getElementById('search-input');
   const searchButton = document.getElementById('search-button');

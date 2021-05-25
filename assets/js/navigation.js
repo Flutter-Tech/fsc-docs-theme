a451c6e1a30994b7578f47a9f65b0e7562e9c06f
crisostomon <72739646+crisostomon@users.noreply.github.com>
2021-05-25 17:15:41 +0100

    Adds console logs to check operation

--- assets/js/navigation.js
+++ assets/js/navigation.js
@@ -21,7 +21,9 @@ document.addEventListener("DOMContentLoaded", function () {
   const scrollLimit = 28;
   const widthLimit = 540;
   var el = document.querySelector(".search-container");
+  console.log(width)
   if (width < widthLimit) {
+    console.log('inside addEventlistener:', width < widthLimit);
     window.addEventListener("scroll", onScroll(scrollY, el, scrollLimit));
   }
 });
@@ -66,11 +68,15 @@ function findElementByClassUrl(cssclass, url) {
 }
 // hides element when scroll Y is above a defined value
 function onScroll(scrollY, el, scrollLimit) {
+  console.log('inside scroll');
   var doc = document.documentElement;
+  console.log('doc:', doc);
+  console.log('scrolly:', scrolly);
   if (doc.scrollTop !== scrollY && doc.scrollTop > scrollLimit) {
     scrollY = doc.scrollTop;
     el.hidden = true;
   } else {
     el.hidden = false;
   }
+  console.log('scrolly out:', scrolly);
 }
